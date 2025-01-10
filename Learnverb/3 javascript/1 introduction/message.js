d7a36f2b6f2386e6548c7c73152e3278afd2e655
Aditya-tops <ap7490944657@gmail.com>
2025-01-10 16:57:33 +0530

    ..
    ...

---
+++ Learnverb/3 javascript/1 introduction/message.js
@@ -0,0 +1,9 @@
+function msg() {
+    alert("Welcome to Aditya, How are you ?")
+    // add comment in js like this......
+
+    // this is a single line comment
+    /*
+    this is  a multiline comment
+    */
+}
