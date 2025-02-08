ad9b7b4ba59be30f30e953048d796fbbe0ce3b60
Aditya-tops <ap7490944657@gmail.com>
2025-02-09 00:12:04 +0530

    .

---
+++ 4 JavaScript/S-JS/50/script.js
@@ -0,0 +1,13 @@
+// function bodyload() {
+//     window.document.images[0].src = "/nehaimage.jpg";  
+//     window.document.forms[0].elements[1].value = "Register";  
+//     window.document.forms[1].elements[1].value = "Login"; 
+// }
+// function bodyload() {
+//     document.getElementById("pic").src = "./nehaimage.jpg"
+//     document.getElementById("form").value="Register"
+// }
+function bodyload() {
+    document.querySelector("#pic").src="./nehaimage.jpg"
+    document.querySelector("#form").value="Register"
+}
