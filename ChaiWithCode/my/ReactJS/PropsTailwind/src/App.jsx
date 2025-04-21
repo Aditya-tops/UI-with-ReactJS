c3a2ddbed671d6b74d27cb364f98ae97e1329f52
Aditya-tops <ap7490944657@gmail.com>
2025-04-21 19:25:29 +0530

    .

---
+++ ChaiWithCode/my/ReactJS/PropsTailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Card from './components/Card'
+function App() {
+  
+  return (
+    <>
+      <div className="card">
+      <h1 className="text-3xl bg-green-500 p-3 rounded-md ">vite with tailwind</h1>
+      <label htmlFor=""></label>
+      <Card/>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ ChaiWithCode/my/ReactJS/PropsTailwind/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+      <div>
+          <img src="https://pokemonletsgo.pokemon.com/assets/img/common/char-pikachu.png" alt="" />
+          <h1 className='text-2xl bg-green-500 p-3 rounded'>Card for photos</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam eius eos id.</p>
+    </div>
+  )
+}
+
+export default Card
